64f8d3386136ec4290cfc4606ba1c2229285082e
yaoyijia <54975658+yaoyijia@users.noreply.github.com>
2025-12-17 03:47:33 +0800

    Update __init__.py

--- components/ntp_server/__init__.py
+++ components/ntp_server/__init__.py
@@ -1,22 +1,33 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_TIME
+from esphome.const import CONF_ID
 
-CODEOWNERS = ["@RobertJN64"]
-DEPENDENCIES = ["time", "network"]
+DEPENDENCIES = ['wifi']
+AUTO_LOAD = []
 
-ntp_server_ns = cg.esphome_ns.namespace("ntp_server")
-NTP_Server = ntp_server_ns.class_("NTP_Server", cg.Component)
+ntp_server_ns = cg.esphome_ns.namespace('ntp_server')
+NTP_Server = ntp_server_ns.class_('NTP_Server', cg.Component)
 
-CONF_GPS_ID = "ntp_server_id"
-CONFIG_SCHEMA = cv.All(
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(NTP_Server),
-        }
-    )
-)
+# 定义新的配置项，用于接收其他组件的引用
+CONF_CUSTOM_GPS_TIME = 'custom_gps_time_id'
+CONF_PPS_SENSOR = 'pps_sensor_id'
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(NTP_Server),
+    cv.Optional(CONF_CUSTOM_GPS_TIME): cv.use_id(cg.esphome_ns.namespace('custom_gps_time').CustomGPSTime),
+    cv.Optional(CONF_PPS_SENSOR): cv.use_id(cg.esphome_ns.namespace('pps_sensor').PPSSensor),
+}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    
+    # 如果配置中提供了custom_gps_time组件的ID，则获取其变量并传递给C++
+    if CONF_CUSTOM_GPS_TIME in config:
+        time_comp = await cg.get_variable(config[CONF_CUSTOM_GPS_TIME])
+        cg.add(var.set_time_component(time_comp))
+    
+    # 如果配置中提供了pps_sensor组件的ID，则获取其变量并传递给C++
+    if CONF_PPS_SENSOR in config:
+        pps_comp = await cg.get_variable(config[CONF_PPS_SENSOR])
+        cg.add(var.set_pps_component(pps_comp))
