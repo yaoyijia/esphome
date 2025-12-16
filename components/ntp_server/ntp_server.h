1047297b22be5a56a32101ed8a09eb74363e4243
yaoyijia <54975658+yaoyijia@users.noreply.github.com>
2025-12-17 04:36:38 +0800

    Update ntp_server.h

--- components/ntp_server/ntp_server.h
+++ components/ntp_server/ntp_server.h
@@ -24,10 +24,34 @@ class NTP_Server : public Component {
   void loop() override;
 
  protected:
+  // 获取高精度时间戳的内部方法
+  bool get_high_precision_timestamp(uint32_t &ntp_seconds, uint32_t &ntp_fraction);
+  
+  // 启动NTP服务器
+  void startNTP();
+  
+  // 处理NTP请求
+  void processNTP();
+  
+  // 确保NTP服务已启动
+  void startNTPIfNeeded();
+
+ private:
   custom_gps_time::CustomGPSTime *time_comp_{nullptr};
   pps_sensor::PPSSensor *pps_comp_{nullptr};
   
-  friend void processNTP(); // 让processNTP函数可以访问私有成员
+  bool first_loop_flag_{true};
+  
+  // UDP服务器
+  WiFiUDP udp_;
+  
+  // 包缓冲区
+  static constexpr int NTP_PORT = 123;
+  static constexpr int NTP_PACKET_SIZE = 48;
+  byte packet_buffer_[NTP_PACKET_SIZE];
+  
+  // Unix时间(1970)转NTP时间(1900)的秒数差
+  static constexpr unsigned long SEVENTY_YEARS = 2208988800UL;
 };
 
 }  // namespace ntp_server
