cc1a6b8c420228f46b68f68ce4f6dece28062130
yaoyijia <54975658+yaoyijia@users.noreply.github.com>
2025-12-17 07:35:42 +0800

    Update __init__.py

--- components/simple_gps_ntp/__init__.py
+++ components/simple_gps_ntp/__init__.py
@@ -3,16 +3,20 @@ import esphome.config_validation as cv
 from esphome.components import uart
 from esphome.const import CONF_ID, CONF_PIN
 
-DEPENDENCIES = ['wifi']
+DEPENDENCIES = ['wifi', 'uart']
 
 simple_gps_ntp_ns = cg.esphome_ns.namespace('simple_gps_ntp')
 SimpleGPSNTPServer = simple_gps_ntp_ns.class_('SimpleGPSNTPServer', cg.Component)
 
 CONF_PPS_PIN = 'pps_pin'
+CONF_UART_ID = 'uart_id'
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(SimpleGPSNTPServer),
     cv.Required(CONF_PPS_PIN): cv.int_range(min=0, max=35),
+    
+    # 可选：指定特定的UART组件
+    cv.Optional(CONF_UART_ID): cv.use_id(uart.UARTComponent),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
@@ -21,8 +25,11 @@ async def to_code(config):
     
     cg.add(var.set_pps_pin(config[CONF_PPS_PIN]))
     
-    # 自动查找可用的UART
-    for uart_key, uart_conf in config.get('uart', {}).items():
-        if isinstance(uart_key, str) and uart_key != 'id':
-            uart_component = await cg.get_variable(uart_conf['id'])
-            cg.add(var.set_uart(uart_component))
+    # 如果指定了特定的UART组件，使用它
+    if CONF_UART_ID in config:
+        uart_component = await cg.get_variable(config[CONF_UART_ID])
+        cg.add(var.set_uart(uart_component))
+    else:
+        # 否则，使用默认的UART组件
+        # 需要确保配置中有uart组件
+        pass
