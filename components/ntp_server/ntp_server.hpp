16e606e903d8611ecc9202caa01e0b9a03db749b
yaoyijia <54975658+yaoyijia@users.noreply.github.com>
2025-12-17 03:23:15 +0800

    Update ntp_server.hpp

--- components/ntp_server/ntp_server.hpp
+++ components/ntp_server/ntp_server.hpp
@@ -8,8 +8,13 @@ namespace ntp_server {
 
 class NTP_Server : public Component {
 public:
+  void set_time_component(CustomGPSTime *comp) { time_comp_ = comp; }
+  void set_pps_component(PPSSensor *comp) { pps_comp_ = comp; }
   void setup() override; // called once
   void loop() override;  // called frequently
+ protected:
+  CustomGPSTime *time_comp_{nullptr};
+  PPSSensor *pps_comp_{nullptr};
 };
 
 } // namespace ntp_server
