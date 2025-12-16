8c1f7e2f5fe6d687cf23153e4a97c1394577cf96
yaoyijia <54975658+yaoyijia@users.noreply.github.com>
2025-12-17 07:34:19 +0800

    Update simple_gps_ntp.h

--- components/simple_gps_ntp/simple_gps_ntp.h
+++ components/simple_gps_ntp/simple_gps_ntp.h
@@ -8,6 +8,7 @@ namespace simple_gps_ntp {
 
 class SimpleGPSNTPServer : public Component {
  public:
+  // 修改方法签名，使用UARTComponent
   void set_uart(esphome::uart::UARTComponent *uart) { uart_ = uart; }
   void set_pps_pin(uint8_t pin) { pps_pin_ = pin; }
   
@@ -24,11 +25,11 @@ class SimpleGPSNTPServer : public Component {
   static void IRAM_ATTR pps_isr();
   
  private:
-  esphome::uart::UARTComponent *uart_ = nullptr;
+  esphome::uart::UARTComponent *uart_ = nullptr;  // 使用 UARTComponent
   uint8_t pps_pin_ = 0;
   
   // GPS数据
-  char gps_buffer_[256];
+  char gps_buffer_[128];  // 减小缓冲区大小
   uint8_t gps_idx_ = 0;
   
   // 时间数据
