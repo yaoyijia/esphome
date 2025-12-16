859f27db4ffa3b9d869bfb6412730f0c0b1c52c5
yaoyijia <54975658+yaoyijia@users.noreply.github.com>
2025-12-16 22:49:48 +0800

    Update __init__.py

--- components/pps_sensor/__init__.py
+++ components/pps_sensor/__init__.py
@@ -1,31 +1,30 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import (
-    CONF_ID,
-    UNIT_SECOND,
-    ICON_PULSE,
-    STATE_CLASS_MEASUREMENT,
-)
+from esphome.const import CONF_ID, CONF_PIN
+
+DEPENDENCIES = []
+AUTO_LOAD = []
 
 pps_sensor_ns = cg.esphome_ns.namespace("pps_sensor")
-PPSSensor = pps_sensor_ns.class_("PPSSensor", sensor.Sensor, cg.PollingComponent)
+PPSSensor = pps_sensor_ns.class_("PPSSensor", cg.PollingComponent)
 
-CONF_PIN = "pin"
-
-CONFIG_SCHEMA = sensor.sensor_schema(
-    PPSSensor,
-    unit_of_measurement=UNIT_SECOND,
-    icon=ICON_PULSE,
-    accuracy_decimals=6,
-    state_class=STATE_CLASS_MEASUREMENT,
-).extend({
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(PPSSensor),
     cv.Required(CONF_PIN): cv.int_range(min=0, max=35),
-}).extend(cv.polling_component_schema("60s"))
+    cv.Optional("interval_sensor"): sensor.sensor_schema(
+        unit_of_measurement="s",
+        icon="mdi:pulse",
+        accuracy_decimals=6,
+    ),
+}).extend(cv.polling_component_schema("1s"))
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await sensor.register_sensor(var, config)
     
     cg.add(var.set_pin(config[CONF_PIN]))
+    
+    if "interval_sensor" in config:
+        sens = await sensor.new_sensor(config["interval_sensor"])
+        cg.add(var.set_sensor(sens))
