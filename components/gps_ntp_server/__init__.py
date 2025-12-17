43b1253a9967bc7b63de9398dd28f54b9d63add2
yaoyijia <54975658+yaoyijia@users.noreply.github.com>
2025-12-17 23:07:05 +0800

    Update __init__.py

--- components/gps_ntp_server/__init__.py
+++ components/gps_ntp_server/__init__.py
@@ -4,19 +4,21 @@ import esphome.config_validation as cv
 from esphome.components import gps
 from esphome.const import CONF_ID
 
-DEPENDENCIES = ['wifi', 'gps']
+DEPENDENCIES = ['wifi']
 
 gps_ntp_server_ns = cg.esphome_ns.namespace('gps_ntp_server')
-GPSNTPServer = gps_ntp_server_ns.class_('GPSNTPServer', cg.Component, gps.GPSListener)
+GPSNTPServer = gps_ntp_server_ns.class_('GPSNTPServer', cg.Component)
 
 CONF_PPS_PIN = 'pps_pin'
 CONF_GPS_ID = 'gps_id'
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(GPSNTPServer),
-    cv.Required(CONF_PPS_PIN): cv.int_range(min=0, max=35),
-    cv.Required(CONF_GPS_ID): cv.use_id(gps.GPS),
-}).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = cv.All(
+    cv.Schema({
+        cv.GenerateID(): cv.declare_id(GPSNTPServer),
+        cv.Required(CONF_PPS_PIN): cv.int_range(min=0, max=35),
+        cv.Optional(CONF_GPS_ID): cv.use_id(gps.GPS),
+    })
+).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
@@ -24,5 +26,7 @@ async def to_code(config):
     
     cg.add(var.set_pps_pin(config[CONF_PPS_PIN]))
     
-    gps_component = await cg.get_variable(config[CONF_GPS_ID])
-    cg.add(var.set_gps(gps_component))
+    if CONF_GPS_ID in config:
+        gps_component = await cg.get_variable(config[CONF_GPS_ID])
+        cg.add(var.set_gps(gps_component))
+
