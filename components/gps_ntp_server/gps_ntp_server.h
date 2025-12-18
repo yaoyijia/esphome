93d4f405010a5fe010226c231c122f60953f0c12
yaoyijia <54975658+yaoyijia@users.noreply.github.com>
2025-12-19 01:06:12 +0800

    Update gps_ntp_server.h

--- components/gps_ntp_server/gps_ntp_server.h
+++ components/gps_ntp_server/gps_ntp_server.h
@@ -9,7 +9,7 @@ namespace gps_ntp_server {
 class GPSNTPServer : public Component {
  public:
   void set_pps_pin(uint8_t pin) { pps_pin_ = pin; }
-  void set_gps(gps::GPS *gps) { gps_ = gps; }  // 可选，仅用于显示状态
+  void set_gps(gps::GPS *gps) { gps_ = gps; }
   
   void setup() override;
   void loop() override;
@@ -24,6 +24,12 @@ class GPSNTPServer : public Component {
   static void IRAM_ATTR pps_interrupt_handler();
   
  private:
+  // 私有函数声明
+  void send_ntp_response(WiFiUDP &udp, IPAddress remote, int remotePort, 
+                        byte *clientTransmit);
+  void handle_ntp_request();
+  void handle_pps();
+  
   // PPS相关
   uint8_t pps_pin_ = 0;
   volatile uint32_t pps_last_edge_us_ = 0;
